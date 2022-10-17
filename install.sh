263328b493fe6de3023eb18b066bded7dc3ac197
x <x>
2022-10-17 09:47:48 +0200

    Remove edge, use chrome for dev

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ install_packages() {
     ## add neovim ppa to get latest version
     sudo add-apt-repository ppa:neovim-ppa/stable
 
-    sudo apt install -y ssh git curl neovim tmux docker.io lua5.1 gnome-tweaks
+    sudo apt install -y ssh git curl neovim tmux tmuxinator docker.io lua5.1 gnome-tweaks
     sudo apt install -y python3-pip python3-venv python3-tk
 }
 
@@ -36,14 +36,6 @@ install_chrome() {
     sudo apt install ./google-chrome-stable_current_amd64.deb
 }
 
-install_edge() {
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common wget -y
-    sudo wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg
-    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
-    sudo apt update
-    sudo apt install microsoft-edge-stable -y
-}
-
 install_teams() {
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
     sudo apt update
@@ -53,13 +45,12 @@ install_teams() {
 install_snaps() {
     sudo snap install --classic code
     sudo snap install --classic slack
-    sudo snap install zoom-client
+    # sudo snap install zoom-client
 }
 
 install_packages
 install_dotfiles
 adduseranddir
-install_edge
 install_chrome
-install_teams
+# install_teams
 install_snaps
